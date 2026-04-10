6c65feec63c0ba8ff0a88cc8d259fa841e5f8ced
wilnba15 <wsvch05@gmail.com>
2026-04-10 17:50:34 -0500

    Corregir tabla vehiculos

--- app/models/vehicle.py
+++ app/models/vehicle.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, String, Text, ForeignKey, DateTime
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 from app.database import Base
